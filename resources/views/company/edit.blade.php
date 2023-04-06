97d8e2b2bfe362ecb268db6a91a39fb542b5602a
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 20:44:44 +0900

    company edit 59

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -30,7 +30,7 @@
             <x-company-input id="recruitEmail" name="採用窓口メールアドレス" type="email" require="true"></x-company-input>
         @else
             @method('patch')
-            {{ $records }}
+            {{ $records -> company_name }}
 {{--            <x-company-input-edit id="companyName" name="企業名" :data="$data->company_name"></x-company-input-edit>--}}
 {{--            <x-company-input-edit id="companyRuby" name="企業名（ふりがな）" type="text" require="true" :data="$data->company_ruby"></x-company-input-edit>--}}
 {{--            <x-company-input-edit id="companyAddress" name="本社所在地" type="text" require="true" :data="$data->company_address"></x-company-input-edit>--}}
