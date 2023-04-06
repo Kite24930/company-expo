7fea081606a2164c8a4c924af243af21157e795e
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 20:37:32 +0900

    company edit 54

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -30,8 +30,10 @@
             <x-company-input id="recruitEmail" name="採用窓口メールアドレス" type="email" require="true"></x-company-input>
         @else
             @method('patch')
-            @foreach($data['records'] as $key => $value)
-                【key:{{ $key }}】value:{{ $value }}<br>
+            @foreach($data['records'] as $record)
+                @foreach($record as $key => $value)
+                    [key:{{ $key }}]value:{{ $value }}<br>
+                @endforeach
             @endforeach
 {{--            <x-company-input-edit id="companyName" name="企業名" :data="$data->company_name"></x-company-input-edit>--}}
 {{--            <x-company-input-edit id="companyRuby" name="企業名（ふりがな）" type="text" require="true" :data="$data->company_ruby"></x-company-input-edit>--}}
