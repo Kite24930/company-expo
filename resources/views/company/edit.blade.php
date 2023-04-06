80114b41230cf5cb68ba120e415f26d54dcc29e5
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 14:15:57 +0900

    company edit 5

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -4,15 +4,13 @@
     <form method="POST" action="/api/company">
         <x-company-input id="companyName" name="企業名"
             @if(isset($data))
-                data="{{ $data -> company_name }}"
-            @endif
+                data="{{ $data -> company_name }}" @endif
         ></x-company-input>
         <div class="mb-3">
             <label for="companyName" class="form-label">企業名</label>
             <input type="text" class="form-control" id="companyName" aria-describedby="companyNameHelp"
             @if(isset($data))
-                value="{{ $data -> company_name }}"
-            @endif
+                value="{{ $data -> company_name }}" @endif
             >
             <div id="companyNameHelp" class="form-text"></div>
         </div>
