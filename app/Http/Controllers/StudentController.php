26a64478b3efc7c4fdf377261d497c4cf7059c14
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 21:35:15 +0900

    student list 1

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Models\Contact;
 
 class StudentController extends Controller
 {
@@ -11,11 +13,18 @@ class StudentController extends Controller
     }
 
     public function list() {
-
+        $data = [
+            'first' => Company::where('section', 1) -> orderBy('booth') -> get(),
+            'second' => Company::where('section', 2) -> orderBy('booth') -> get()
+        ];
+        return view('student.list', $data);
     }
 
     public function detail($id) {
-
+        $data = [
+            'record' => Company::where('company_id', $id) -> first(),
+        ];
+        return view('student.detail', $data);
     }
 
     public function contact($id) {
--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -1,1 +1,63 @@
-<?php
+<x-template title="参加企業一覧">
+    <div class="container-md">
+        <h1>参加企業一覧</h1>
+    </div>
+    <div class="container-md d-flex justify-content-center align-items-center">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2>【第一部（9:30〜11:30）】</h2>
+            <div class="d-flex justify-content-center align-items-center">
+                @foreach($first as $record)
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">【No.{{ $record -> booth }}】{{ $record -> company_name }}</h5>
+                            <p class="card-text">{{ $record -> company_PR }}</p>
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    【業種】{{ $record -> category }}
+                                </li>
+                                <li class="list-group-item">
+                                    【募集職種】{{ $record -> recruit_type }}
+                                </li>
+                                <li class="list-group-item">
+                                    【資本金】{{ $record -> capital }}
+                                </li>
+                                <li class="list-group-item">
+                                    【売上高】{{ $record -> sales }}
+                                </li>
+                            </ul>
+                            <a href="/student/detail/{{ $record -> company_id }}" class="btn btn-primary">詳細ページへ</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2>【第二部（12:30〜14:30）】</h2>
+            <div class="d-flex justify-content-center align-items-center">
+                @foreach($second as $record)
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">【No.{{ $record -> booth }}】{{ $record -> company_name }}</h5>
+                            <p class="card-text">{{ $record -> company_PR }}</p>
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    【業種】{{ $record -> category }}
+                                </li>
+                                <li class="list-group-item">
+                                    【募集職種】{{ $record -> recruit_type }}
+                                </li>
+                                <li class="list-group-item">
+                                    【資本金】{{ $record -> capital }}
+                                </li>
+                                <li class="list-group-item">
+                                    【売上高】{{ $record -> sales }}
+                                </li>
+                            </ul>
+                            <a href="/student/detail/{{ $record -> company_id }}" class="btn btn-primary">詳細ページへ</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-template>
