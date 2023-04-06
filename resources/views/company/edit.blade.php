6f38ca1e8273ecf17cd027aa88565985e3e436e5
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 14:32:07 +0900

    company edit 16

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -2,12 +2,11 @@
     <h1>企業情報編集ページ</h1>
     <p>Webページに記載する企業情報を登録、編集することができます。</p>
     <form method="POST" action="/api/company">
-        {{ $count }}
-{{--        @if(empty($data))--}}
-{{--            <x-company-input id="companyName" name="企業名"></x-company-input>--}}
-{{--        @else--}}
-{{--            <x-company-input-edit id="companyName" name="企業名" :data="$data->company_name"></x-company-input-edit>--}}
-{{--        @endif--}}
+        @if($count === 0)
+            <x-company-input id="companyName" name="企業名"></x-company-input>
+        @else
+            <x-company-input-edit id="companyName" name="企業名" :data="$data->company_name"></x-company-input-edit>
+        @endif
     </form>
     @vite(['resources/js/edit.js'])
 </x-template>
