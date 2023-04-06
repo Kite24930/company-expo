3a75e349858d7f9116d5a593e9514f3004a274e4
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 14:19:18 +0900

    company edit 7

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -3,10 +3,5 @@
     <p>Webページに記載する企業情報を登録、編集することができます。</p>
     <form method="POST" action="/api/company">
         <x-company-input id="companyName" name="企業名" @if(isset($data)) data="{{ $data -> company_name }}" @endif ></x-company-input>
-        <div class="mb-3">
-            <label for="companyName" class="form-label">企業名</label>
-            <input type="text" class="form-control" id="companyName" aria-describedby="companyNameHelp" @if(isset($data)) value="{{ $data -> company_name }}" @endif >
-            <div id="companyNameHelp" class="form-text"></div>
-        </div>
     </form>
 </x-template>
