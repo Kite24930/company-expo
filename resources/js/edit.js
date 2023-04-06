4c4f685b88ecb59486f81db6155c3b2e7bf3dc78
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-06 16:31:00 +0900

    company edit 21

--- resources/js/edit.js
+++ resources/js/edit.js
@@ -29,5 +29,90 @@ const recruitEmail = document.getElementById('recruitEmail');
 editBtn.addEventListener('click', () => {
     let check = true;
     let msg = '';
+    if (companyName.value === '') {
+        check = false;
+        msg += '・企業名を入力してください。\n';
+    }
+    if (companyRuby.value === '') {
+        check = false;
+        msg += '・企業名（ふりがな）を入力してください。\n';
+    }
+    if (companyAddress.value === '') {
+        check = false;
+        msg += '・本社所在地を入力してください。\n';
+    }
+    if (establishment.value === '') {
+        check = false;
+        msg += '・設立年月日が入力されていません。\n'
+    }
+    if (capital.value === '') {
+        check = false;
+        msg += '・資本金が入力されていません。\n';
+    } else if (capital.value.match(/[^0-9]+/) === null) {
+        check = false;
+        msg += '・資本金に数字以外が入力されています。\n';
+    }
+    if (sales.value === '') {
+        check = false;
+        msg += '・売上高が入力されていません。\n';
+    } else if (sales.value.match(/[^0-9]+/) === null) {
+        check = false;
+        msg += '・売上高に数字以外が入力されています。\n';
+    }
+    if (employees.value === '') {
+        check = false;
+        msg += '・従業員数が入力されていません。\n';
+    } else if (employees.value.match(/[^0-9]+/) === null) {
+        check = false;
+        msg += '・従業員数に数字以外が入力されています。\n';
+    }
+    if (companyHP.value === '') {
+        check = false;
+        msg += '・会社HP等が入力されていません。\n';
+    }
+    if (category.value === '') {
+        check = false;
+        msg += '・業種・業態が入力されていません\n';
+    }
+    if (description.value === '') {
+        check = false;
+        msg += '・事業内容が入力されていません。\n';
+    }
+    if (companyPR.value === '') {
+        check = false;
+        msg += '・企業PRが入力されていません。\n';
+    }
+    if (schoolmate.value === '') {
+        check = false;
+        msg += '・三重大学卒業生入社人数が入力されていません。\n';
+    } else if (schoolmate.value.match(/[^0-9]+/) === null) {
+        check = false;
+        msg += '・三重大学卒業生入社人数に数字以外が入力されています。\n';
+    }
+    if (recruitType.value === '') {
+        check = false;
+        msg += '・募集職種が入力されていません。\n';
+    }
+    if (location.value === '') {
+        check = false;
+        msg += '・勤務地が入力されていません。\n';
+    }
+    if (recruitName.value === '') {
+        check = false;
+        msg += '・採用窓口担当者が入力されていません。\n';
+    }
+    if (recruitRuby.value === '') {
+        check = false;
+        msg += '・採用窓口担当者（ふりがな）が入力されていません。\n';
+    }
+    if (recruitEmail.value === '') {
+        check = false;
+        msg += '・採用窓口メールアドレスが入力されていません。\n';
+    }
 
+    if (check) {
+        document.getElementById('companyForm').submit();
+    } else {
+        window.alert(msg);
+    }
 })
--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -11,16 +11,16 @@
             <x-company-input id="companyRuby" name="企業名（ふりがな）" type="text" require="true"></x-company-input>
             <x-company-input id="companyAddress" name="本社所在地" type="text" require="true"></x-company-input>
             <x-company-input id="establishment" name="設立年月日" type="date" require="true"></x-company-input>
-            <x-company-input id="capital" name="資本金" type="tel" require="true">単位：百万円※数字のみ入力してください（例：資本金4000万円→40）</x-company-input>
-            <x-company-input id="sales" name="売上高" type="tel" require="true">単位：百万円※数字のみ入力してください（例：売上高1億7000万円→170）</x-company-input>
-            <x-company-input id="employees" name="従業員数" type="tel" require="true">単位：人※数字のみ入力してください</x-company-input>
-            <x-company-input id="companyHP" name="会社HP等" type="url" require="true">会社のHPや採用ページのURLを入力してください。</x-company-input>
+            <x-company-input id="capital" name="資本金" type="number" require="true">単位：百万円※数字のみ入力してください（例：資本金4000万円→40）</x-company-input>
+            <x-company-input id="sales" name="売上高" type="number" require="true">単位：百万円※数字のみ入力してください（例：売上高1億7000万円→170）</x-company-input>
+            <x-company-input id="employees" name="従業員数" type="number" require="true">単位：人※数字のみ入力してください</x-company-input>
+            <x-company-input id="companyHP" name="会社HP等" type="number" require="true">会社のHPや採用ページのURLを入力してください。</x-company-input>
             <x-company-input id="category" name="業種・業態" type="text" require="true"></x-company-input>
             <x-company-textarea id="description" name="事業内容" require="true"></x-company-textarea>
             <x-company-textarea id="companyPR" name="企業PR" require="true"></x-company-textarea>
-            <x-company-input id="schoolmate" name="三重大学卒業生入社人数" type="tel" require="true">単位：人※数字のみ入力してください</x-company-input>
+            <x-company-input id="schoolmate" name="三重大学卒業生入社人数" type="number" require="true">単位：人※数字のみ入力してください</x-company-input>
             <x-company-input id="recruitType" name="募集職種" type="text" require="true"></x-company-input>
-            <x-company-input id="recruitPlannedNumber" name="採用予定人数" type="tel">単位：人※数字のみ入力してください</x-company-input>
+            <x-company-input id="recruitPlannedNumber" name="採用予定人数" type="number">単位：人※数字のみ入力してください</x-company-input>
             <x-company-input id="location" name="勤務地" type="text" require="true"></x-company-input>
             <x-company-input id="target" name="対象学生" type="text">※理系学部生、文系学部生、理系院生、文系院生より入力してください</x-company-input>
             <x-company-input id="recruitDepartment" name="採用窓口部署名" type="text"></x-company-input>
@@ -34,16 +34,16 @@
             <x-company-input-edit id="companyRuby" name="企業名（ふりがな）" type="text" require="true" :data="$data->company_ruby"></x-company-input-edit>
             <x-company-input-edit id="companyAddress" name="本社所在地" type="text" require="true" :data="$data->company_address"></x-company-input-edit>
             <x-company-input-edit id="establishment" name="設立年月日" type="date" require="true" :data="$data->establishment"></x-company-input-edit>
-            <x-company-input-edit id="capital" name="資本金" type="tel" require="true" :data="$data->capital">単位：百万円※数字のみ入力してください（例：資本金4000万円→40）</x-company-input-edit>
-            <x-company-input-edit id="sales" name="売上高" type="tel" require="true" :data="$data->sales">単位：百万円※数字のみ入力してください（例：売上高1億7000万円→170）</x-company-input-edit>
-            <x-company-input-edit id="employees" name="従業員数" type="tel" require="true" :data="$data->employees">単位：人※数字のみ入力してください</x-company-input-edit>
+            <x-company-input-edit id="capital" name="資本金" type="number" require="true" :data="$data->capital">単位：百万円※数字のみ入力してください（例：資本金4000万円→40）</x-company-input-edit>
+            <x-company-input-edit id="sales" name="売上高" type="number" require="true" :data="$data->sales">単位：百万円※数字のみ入力してください（例：売上高1億7000万円→170）</x-company-input-edit>
+            <x-company-input-edit id="employees" name="従業員数" type="number" require="true" :data="$data->employees">単位：人※数字のみ入力してください</x-company-input-edit>
             <x-company-input-edit id="companyHP" name="会社HP等" type="url" require="true" :data="$data->company_HP">会社のHPや採用ページのURLを入力してください。</x-company-input-edit>
             <x-company-input-edit id="category" name="業種・業態" type="text" require="true" :data="$data->category"></x-company-input-edit>
             <x-company-textarea-edit id="description" name="事業内容" require="true" :data="$data->description"></x-company-textarea-edit>
             <x-company-textarea-edit id="companyPR" name="企業PR" require="true" :data="$data->company_PR"></x-company-textarea-edit>
-            <x-company-input-edit id="schoolmate" name="三重大学卒業生入社人数" type="tel" require="true" :data="$data->schoolmate">単位：人※数字のみ入力してください</x-company-input-edit>
+            <x-company-input-edit id="schoolmate" name="三重大学卒業生入社人数" type="number" require="true" :data="$data->schoolmate">単位：人※数字のみ入力してください</x-company-input-edit>
             <x-company-input-edit id="recruitType" name="募集職種" type="text" require="true" :data="$data->recruit_type"></x-company-input-edit>
-            <x-company-input-edit id="recruitPlannedNumber" name="採用予定人数" type="tel" :data="$data->recruit_planned_number">単位：人※数字のみ入力してください</x-company-input-edit>
+            <x-company-input-edit id="recruitPlannedNumber" name="採用予定人数" type="number" :data="$data->recruit_planned_number">単位：人※数字のみ入力してください</x-company-input-edit>
             <x-company-input-edit id="location" name="勤務地" type="text" require="true" :data="$data->location"></x-company-input-edit>
             <x-company-input-edit id="target" name="対象学生" type="text" :data="$data->target">※理系学部生、文系学部生、理系院生、文系院生より入力してください</x-company-input-edit>
             <x-company-input-edit id="recruitDepartment" name="採用窓口部署名" type="text" :data="$data->recruit_department"></x-company-input-edit>
