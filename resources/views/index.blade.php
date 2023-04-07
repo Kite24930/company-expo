a1551e07fc6b1d1584df7c793dd8c7c5c45cdec5
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-07 20:19:40 +0900

    student detail 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
                             <p class="card-text">[企業PR]
                                 <br>
                                 @if(mb_strlen($record->company_PR) > 40)
-                                    {{ substr($record->company_PR, 0, 40) }}...
+                                    {{ mb_substr($record->company_PR, 0) }}...
                                 @else
                                     {{ $record -> company_PR }}
                                 @endif
