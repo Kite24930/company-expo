465190facddd9e9ec1118dce87ee1b2a579d80f5
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-10 19:20:39 +0900

    student contact 16

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -44,7 +44,34 @@ class StudentController extends Controller
                 'detail' => $request->detail,
             ]);
             DB::commit();
+            $company = Company::where('company_id', $request->companyId)->first();
 //            メール送信の処理
+            $to = $request->studentEmail;
+            $subject = '【自動送信】'.$company->company_name.' お問い合わせ内容';
+            $message = $request->studentDepartment.' '.$request->studentName."様\r\n\r\n";
+            $message .= 'この度はお問い合わせいただきありがとうございます。'."\r\n".'お問い合わせいただいた内容をご確認ください。'."\r\n".'後日、お問い合わせいただいた企業よりご回答が送信される予定です。'."\r\n".'もし時間を置いてもご返答がない場合は、直接企業様の採用担当窓口へご連絡ください。'."\r\n\r\n";
+            $message .= '【お問い合わせ内容】'."\r\n".'[お問い合わせ企業様]'.$company->company_name."\r\n".'[学籍番号]'.$request->studentNumber."\r\n".'[学部・研究科]'.$request->studentDepartment."\r\n".'[学年]'.$request->studentGrade."\r\n".'[氏名]'.$request->studentName."\r\n".'[メールアドレス]このメールが届いているメールアドレス'."\r\n".'[件名]'.$request->title."\r\n".'[お問い合わせ内容]'.$request->detail."\r\n\r\n".'※このメールは送信専用ですので、ご返信いただいていも返信できません。'."\r\n\r\n".'2023年5月 学内合同企業説明会';
+            $headers = 'From:no-reply@company-expo.mie-projectm.com';
+            mb_send_mail($to, $subject, $message, $headers);
+            $to = $company->recruit_email;
+            $subject = '【自動送信】学生より'.$company->company_name.'様へのお問い合わせ内容';
+            $message = $company->company_name.' '.$company->recruit_name."様\r\n\r\n";
+            $message .= '2023年5月に開催致しました三重大学工学部同窓会主催 学内合同企業説明会にて、学生より問い合わせがございました。'."\r\n".'お問い合わせいただいた内容をご確認ください。'."\r\n".'お手数をおかけ致しますが、問い合わせ内容のご返答をよろしくお願い致します。'."\r\n\r\n";
+            $message .= '【お問い合わせ内容】'."\r\n".'[お問い合わせ企業様]'.$company->company_name."\r\n".'[学籍番号]'.$request->studentNumber."\r\n".'[学部・研究科]'.$request->studentDepartment."\r\n".'[学年]'.$request->studentGrade."\r\n".'[氏名]'.$request->studentName."\r\n".'[メールアドレス]このメールが届いているメールアドレス'."\r\n".'[件名]'.$request->title."\r\n".'[お問い合わせ内容]'.$request->detail."\r\n\r\n".'※このメールは送信専用ですので、ご返信いただいていも返信できません。'."\r\n\r\n".'2023年5月 学内合同企業説明会';
+            $headers = 'From:no-reply@company-expo.mie-projectm.com';
+            mb_send_mail($to,  $subject, $message, $headers);
+            $data = [
+                'company_id' => $request -> companyId,
+                'company_name' => $company -> company_name,
+                'student_number' => $request -> studentNumber,
+                'student_department' => $request -> studentDepartment,
+                'student_grade' => $request -> studentGrade,
+                'student_name' => $request -> studentName,
+                'student_email' => $request -> studentEmail,
+                'title' => $request -> title,
+                'detail' => $request -> detail,
+            ];
+            return view('student.contact-result', $data);
         } catch (\Exception $e) {
             DB::rollBack();
             return ['msg' => $e];
---
+++ resources/views/student/contact-result.blade.php
@@ -0,0 +1,45 @@
+<x-template :title="$record -> company_name . ' お問い合わせ'">
+    <div class="d-flex align-items-center">
+        <h1>{{ $record -> company_name }} 問い合わせ完了</h1>
+    </div>
+    <div class="container-md d-flex justify-content-center align-items-center flex-column my-3">
+        <div>
+            お問い合わせが完了しました。以下の内容が企業様に送信されています。同様の内容を入力していただいたメールアドレスにも送信してありますので、そちらでもご確認ください。
+        </div>
+        <ul class="list-group container-md m-o p-3">
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">企業名</p>
+                <p class="form-label">{{ $company_name }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">学籍番号</p>
+                <p class="form-label">{{ $student_number }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">学部・研究科</p>
+                <p class="form-label">{{ $student_department }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">学年</p>
+                <p class="form-label">{{ $student_grade }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">氏名</p>
+                <p class="form-label">{{ $student_name }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">件名</p>
+                <p class="form-label">{{ $title }}</p>
+            </li>
+            <li class="list-group-item">
+                <p class="form-text border-bottom d-inline-block">お問い合わせ内容</p>
+                <p class="form-label">{{ $detail }}</p>
+            </li>
+        </ul>
+        <div class="text-center">
+            <a href="/detail/{{ $company_id }}" class="btn btn-success text-white">{{ $company_name }} 詳細ページに戻る</a>
+            <a href="/" class="btn btn-primary text-white">TOPページに戻る</a>
+        </div>
+    </div>
+    @vite(['resources/js/detail.js'])
+</x-template>
