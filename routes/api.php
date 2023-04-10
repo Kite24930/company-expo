e948940482e2689a28819c16c7b831e508e0d891
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-10 11:43:23 +0900

    student contact 2

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/company', [CompanyController::class, 'create']);
 Route::patch('/company', [CompanyController::class, 'fix']);
-Route::post('/contact', [StudentController::class, ])
+Route::post('/contact', [StudentController::class, 'create']);
