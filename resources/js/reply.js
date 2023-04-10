ebcac55ce7bfb650cde3aba0584632df856b97c8
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-10 20:50:26 +0900

    company reply 6

--- resources/js/reply.js
+++ resources/js/reply.js
@@ -1,4 +1,4 @@
-import '/resources/css/detail.css';
+import '/resources/css/reply.css';
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
