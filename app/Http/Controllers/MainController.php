20fd26bb3db34d68fdae154e1787fa9d93b1bb68
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-04-13 19:10:17 +0900

    index fix 16

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -22,11 +22,13 @@ class MainController extends Controller
 
     public function index() {
         $today = new \DateTime();
-        $publicTime = new \DateTime('2023-04-17');
+        $publicTime = new \DateTime('2023-04-24');
         $today < $publicTime ? $dateCheck = false : $dateCheck = true;
         $data = [
-            'first' => Company::where('section', 1) ->orwhere('section', 3) -> orderBy('booth') -> get(),
-            'second' => Company::where('section', 2) -> orwhere('section', 3) -> orderBy('booth') -> get(),
+            'records' => [
+                Company::where('section', 1) ->orwhere('section', 3) -> orderBy('booth') -> get(),
+                Company::where('section', 2) -> orwhere('section', 3) -> orderBy('booth') -> get(),
+            ],
             'dateCheck' => true,
         ];
         return view('index', $data);
